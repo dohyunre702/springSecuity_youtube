21104e01a85554ae9d1d6a1c37ccbba1c5667d44
dohyu <dohyunre702@gmail.com>
2022-12-02 14:41:57 +0900

    [update] uri오타수정

--- src/main/java/com/example/springsecurity/controller/UserController.java
+++ src/main/java/com/example/springsecurity/controller/UserController.java
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/vq/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto) {
+        userService.join(dto.getUserName(), dto.getPassword());
         return ResponseEntity.ok().body("회원가입이 성공했습니다.");
     }
 }
